61d9b4e5ebf694fe5ebddd20ca98b2d0c62d1e75
Peres Juliao <peresjuliao@gmail.com>
2023-01-10 00:20:45 -0300

    Product repository, service, resource, GET, ok

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -35,7 +35,7 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Optional<Product> obj = productRepository.findById(id);
         Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-        return new ProductDTO(entity);
+        return new ProductDTO(entity, entity.getCategories());
     }
 
     @Transactional
