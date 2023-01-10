3bb17ca856e8a065c3ff92178ba7d22ffd6c3183
Peres Juliao <peresjuliao@gmail.com>
2023-01-09 23:15:36 -0300

    ProductDTO and ProductRepository

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,96 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ProductDTO implements Serializable {
+
+    private static final long serialVersionUID = -9035476276250408464L;
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String imgUrl;
+
+    private Instant date;
+
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, Double price, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.price = entity.getPrice();
+        this.imgUrl = entity.getImgUrl();
+        this.date = entity.getDate();
+    }
+
+    public ProductDTO(Product entity, Set<Category> categories) {
+        this(entity); // Chama o construtor que recebe por parametro o Product
+        categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<CategoryDTO> categories) {
+        this.categories = categories;
+    }
+}
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -0,0 +1,70 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRespository;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public Page<Pro> findAllPaged(PageRequest pageRequest) {
+        Page<Category> list = productRepository.findAll(pageRequest);
+        return list.map(CategoryDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = categoryRespository.findById(id);
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = categoryRespository.save(entity);
+        return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = categoryRespository.getOne(id);
+            entity.setName(dto.getName());
+            entity = categoryRespository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            categoryRespository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+
+
+}
